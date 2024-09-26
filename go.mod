8e9e51daee35be5f8cd99a6c28abaf90fa02c30e
emil.shalamberidze <emil.shalamberidze@gmail.com>
2024-09-26 15:31:20 +0400

    go mod file corrected to use github url

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module fighterapplication
+module github.com/3milly4ever/fighter-application
 
 go 1.20
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -3,9 +3,11 @@ package parser
 import (
 	"regexp"
 	"strconv"
+
+	"github.com/3milly4ever/fighter-application/internal/model"
 )
 
-func parseHeight(value string, fighter *Fighter) {
+func parseHeight(value string, fighter *model.Fighter) {
 	// Example value: "5'11\" (180cm)"
 	re := regexp.MustCompile(`(\d+)'(\d+)" \((\d+)cm\)`)
 	matches := re.FindStringSubmatch(value)
@@ -18,7 +20,7 @@ func parseHeight(value string, fighter *Fighter) {
 	}
 }
 
-func parseWeight(value string, fighter *Fighter) {
+func parseWeight(value string, fighter *model.Fighter) {
 	// Example value: "170 lbs (77 kg)"
 	re := regexp.MustCompile(`(\d+) lbs \((\d+) kg\)`)
 	matches := re.FindStringSubmatch(value)
